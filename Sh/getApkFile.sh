e26e34245ef88bfc6cb996f2bb14ef25ff068f67
shenyongxing <shenxing583@gmail.com>
2016-12-19 17:08:00 +0800

    getApkFile script

---
+++ Sh/getApkFile.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+# get the apk install file
+
+adb shell "su -c '
+        cd /data/app
+        ls
+    '
+"
+read -p "please select the package you want retrieve: " targetPkgName
+
+#targetPkgName="com.blue.battery-2.apk"
+#targetPkgName="com.snapchat.android-1.apk"
+
+adb shell "su -c '
+        cd /data/app
+        ls
+        cp $targetPkgName /sdcard
+    '
+"
+
+if [ -e $targetPkgName ];then
+    rm -r $targetPkgName
+fi
+
+adb pull /sdcard/$targetPkgName ./
+
+if [ $? -ne 0 ]; then
+    echo "retrieve apk failed"
+else
+    echo "retrieve apk succeed"
+fi
